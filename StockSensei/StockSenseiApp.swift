548b19e7c5dddf671187d2cb776fe9369895907a
Ava Ginsberg <avaginsberg08@gmail.com>
2023-08-01 11:43:55 -0400

    Initial Commit

---
+++ StockSensei/StockSenseiApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockSenseiApp.swift
+//  StockSensei
+//
+//  Created by scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct StockSenseiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
