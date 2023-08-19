9674b8b541685005be192da19b377172ce933990
Ava Ginsberg <avaginsberg08@gmail.com>
2023-08-19 19:36:15 -0400

    content view

--- main pages/ContentView.swift
+++ main pages/ContentView.swift
@@ -1,63 +1,56 @@
-//
-//  ContentView.swift
-//  StockSensei
-//
-//  Created by scholar on 8/1/23.
-//
-
 import SwiftUI
 import CoreData
+
 struct ContentView: View {
-    //@EnvironmentObject var userData: UserData
-    @State var name:String
+    @State var name: String
     @State var OwnAmzn = false
-    @State var selection:String
+    @State var selection: String
     @State private var isOwnAmzn: Bool = false
-    @State var numselection:Double = 100000
-    @Binding var coinAmount:Double 
-    var body: some View{
-        //AMZN(ownAmzn: $isOwnAmzn, showButton: false)
-        //Invest(ownAmzn: $isOwnAmzn)
-        NavigationStack{
-            VStack(){
-                Text("Welcome to StockSensei")
-                    .font(.largeTitle)
-                Image("stocks")
-                    .resizable(resizingMode: .stretch)
-                    .aspectRatio(contentMode: .fit)
-                HStack(){
-                    NavigationLink(destination: login(name: $name, selection: $selection, numselection: $numselection)) {
-                                        Text("Login")
-                                            .foregroundColor(Color.black)
-                                            .background(Color.green)
-                                            .cornerRadius(10)
-                        
-                    }//navlink closing
-                    NavigationLink(destination:learn()){
-                        Text("Learn")
-                        
-                    }//navlink closing
-                    NavigationLink(destination:Funds(name:$name, selection:$selection, numselection:$numselection)){
-                        Text("Funds")
-                    }//navlink closing funds
-                    NavigationLink(destination:Invest(ownAmzn:$isOwnAmzn, coinAmount:$coinAmount)){
-                        Text("Invest")
-                    }//navlink closing invest
-                }//hstack closing
+    @State var numselection: Double = 100000
+    @Binding var coinAmount: Double
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color(red: 0.827, green: 0.827, blue: 0.827).edgesIgnoringSafeArea(.all) // Blue background covering the entire screen
                 
-            }//vstack closing
+                VStack {
+                    Text("Welcome to StockSensei")
+                        .font(.largeTitle)
+                    Image("stocks")
+                        .resizable(resizingMode: .stretch)
+                        .aspectRatio(contentMode: .fit)
+                    HStack {
+                        NavigationLink(destination: login(name: $name, selection: $selection, numselection: $numselection)) {
+                            Text("Login")
+                                .foregroundColor(Color.black)
+                                .frame(width: 80, height: 80)
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                        NavigationLink(destination: learn()) {
+                            Text("Learn")
+                        }
+                        NavigationLink(destination: Funds(name: $name, selection: $selection, numselection: $numselection)) {
+                            Text("Funds")
+                        }
+                        NavigationLink(destination: Invest(ownAmzn: $isOwnAmzn, coinAmount: $coinAmount)) {
+                            Text("Invest")
+                        }
+                    }
+                }
+                .padding()
+            }
             .navigationTitle("🏠")
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarHidden(false)
-        } //navstack closing
-    }//view closing
-}//struct closing
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView(name: "John Doe", selection: "N/A", numselection: 100000.0, coinAmount:.constant(0.0))
-            //.environmentObject(UserData()) // Add the environment object for UserData
+        }
     }
 }
 
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(name: "John Doe", selection: "N/A", numselection: 100000.0, coinAmount: .constant(0.0))
+    }
+}
 
